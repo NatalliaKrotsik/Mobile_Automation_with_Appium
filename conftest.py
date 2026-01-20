26f755570845e030c0d4476cfacb87dfc3983613
Natallia Krotsik <n.krotsik@andersenlab.com>
2026-01-20 17:53:36 +0100

    feat: set main activity as entry point for automation

--- conftest.py
+++ conftest.py
@@ -3,5 +3,5 @@ from appium.options.android import UiAutomator2Options
 
 options = UiAutomator2Options()
 options.app_package = "com.andersenlab.easyfin"
-options.app_activity = "com.andersenlab.easyfin.uikit.UIKitPlaybookActivity"
+options.app_activity = "com.andersenlab.easyfin.presentation.activity.MainActivity"
 options.app = "C:/Users/n.krotsik/Desktop/EasyFin-Android/easy-fin-android/app/build/outputs/apk/debug/app-debug.apk"
