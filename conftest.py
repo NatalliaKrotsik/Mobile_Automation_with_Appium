8ed35d0317477e94f9bbb83648519352fcebc0a3
Natallia Krotsik <n.krotsik@andersenlab.com>
2026-01-20 17:46:44 +0100

    fix: update conftest with correct apk path and ignore build artifacts

---
+++ conftest.py
@@ -0,0 +1,7 @@
+# Example of how those lines should sit in your actual function
+from appium.options.android import UiAutomator2Options
+
+options = UiAutomator2Options()
+options.app_package = "com.andersenlab.easyfin"
+options.app_activity = "com.andersenlab.easyfin.uikit.UIKitPlaybookActivity"
+options.app = "C:/Users/n.krotsik/Desktop/EasyFin-Android/easy-fin-android/app/build/outputs/apk/debug/app-debug.apk"
